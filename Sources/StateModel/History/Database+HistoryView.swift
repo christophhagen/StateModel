f44b518d74b92a3671fc1bb7af34023b5b7bb9c3
Christoph Hagen <github@christophhagen.de>
2025-09-18 15:28:14 +0200

    Improve history view

--- Sources/StateModel/History/Database+HistoryView.swift
+++ Sources/StateModel/History/Database+HistoryView.swift
@@ -4,8 +4,10 @@ extension HistoryDatabase {
 
     /**
      Get a view of the database at a specific instant in time.
+     - Parameter date: The instant in time for which to view the database
+     - Returns: The view of the database at the given date
      */
     public func view(at date: Date) -> HistoryView<ModelKey, InstanceKey, PropertyKey> {
-        .init(wrapped: self, date: date)
+        .init(wrapped: self, at: date)
     }
 }
--- Sources/StateModel/History/HistoryView.swift
+++ Sources/StateModel/History/HistoryView.swift
@@ -5,27 +5,40 @@ import Foundation
  */
 public final class HistoryView<ModelKey,InstanceKey,PropertyKey>: Database<ModelKey,InstanceKey,PropertyKey> where ModelKey: ModelKeyType, InstanceKey: InstanceKeyType, PropertyKey: PropertyKeyType {
 
-    unowned let wrapped: HistoryDatabase<ModelKey,InstanceKey,PropertyKey>
+    /// The database that is accessed
+    private let wrapped: HistoryDatabase<ModelKey,InstanceKey,PropertyKey>
 
-    private let date: Date
+    /// The date at which the database is accessed
+    private var date: Date
 
-    init(wrapped: HistoryDatabase<ModelKey, InstanceKey, PropertyKey>, date: Date) {
+    /**
+     Create a new history view
+     */
+    init(wrapped: HistoryDatabase<ModelKey, InstanceKey, PropertyKey>, at date: Date) {
         self.wrapped = wrapped
         self.date = date
     }
 
     public override func get<Value>(model: ModelKey, instance: InstanceKey, property: PropertyKey) -> Value? where Value : Decodable, Value : Encodable {
-        wrapped.get(model: model, instance: instance, property: property, at: self.date)?.value
+        wrapped.get(model: model, instance: instance, property: property, at: date)?.value
     }
 
     public override func set<Value>(_ value: Value, model: ModelKey, instance: InstanceKey, property: PropertyKey) where Value : Decodable, Value : Encodable {
-        // TODO: Throw a fatal error when writing data in a history view?
-        wrapped.set(value, model: model, instance: instance, property: property, at: self.date)
+        // Note: In a history view, we don't allow writing to the database, and just ignore all updates
+        // wrapped.set(value, model: model, instance: instance, property: property, at: date)
     }
 
     public override func all<T>(model: ModelKey, where predicate: (InstanceKey, InstanceStatus) -> T?) -> [T] {
-        wrapped.all(model: model, at: self.date) { instance, status, _ in
+        wrapped.all(model: model, at: date) { instance, status, _ in
             predicate(instance, status)
         }
+    }
+
+    /**
+     Move the history view to the given date.
+     - Parameter date: The date at which to view the database
+     */
+    public func moveView(to date: Date) {
+        self.date = date
     }
 }
