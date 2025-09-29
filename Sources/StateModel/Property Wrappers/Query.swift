b34b74418cece190dd89269e4c9317b51ef57c79
Christoph Hagen <github@christophhagen.de>
2025-09-29 17:06:48 +0200

    Create Query.swift

---
+++ Sources/StateModel/Property Wrappers/Query.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+@MainActor
+@available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
+@propertyWrapper
+public struct Query<Result: ModelProtocol>: @MainActor DynamicProperty {
+
+    @EnvironmentObject
+    private var database: ObservableDatabase<Result.ModelKey, Result.InstanceKey, Result.PropertyKey>
+
+    @StateObject
+    private var observer: QueryManager<Result> = .init(database: nil)
+
+    public init() { }
+
+    public var wrappedValue: [Result] {
+        observer.results
+    }
+
+    mutating public func update() {
+        observer.update(database: database)
+    }
+}
