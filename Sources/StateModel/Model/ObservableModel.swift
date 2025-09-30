039e77b6624646070b5fd3ea9012fc544989020f
Christoph Hagen <github@christophhagen.de>
2025-09-30 23:38:55 +0200

    Attempt one more fix

--- Sources/StateModel/Model/ObservableModel.swift
+++ Sources/StateModel/Model/ObservableModel.swift
@@ -28,4 +28,7 @@ public typealias ObservableModel = ObservableBaseModel & ModelProtocol
 @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 open class ObservableBaseModel: BaseModel, ObservableObject {
 
+    public override init(database: any Database, id: BaseModel.InstanceKey) {
+        super.init(database: database, id: id)
+    }
 }
