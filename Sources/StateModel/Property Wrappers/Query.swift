5b09d295b9a9a4c031e37fa55f98833d7a0bf426
Christoph Hagen <github@christophhagen.de>
2025-10-01 17:40:42 +0200

    Add query sorting and filtering

--- Sources/StateModel/Property Wrappers/Query.swift
+++ Sources/StateModel/Property Wrappers/Query.swift
@@ -1,5 +1,10 @@
 import SwiftUI
 
+public enum QuerySortOrder {
+    case ascending
+    case descending
+}
+
 @MainActor
 @available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
 @propertyWrapper
@@ -9,9 +14,47 @@ public struct Query<Result: ModelProtocol>: @MainActor DynamicProperty {
     private var database: ObservableDatabase
 
     @StateObject
-    private var observer: QueryManager<Result> = .init(database: nil)
+    private var observer: QueryManager<Result>
 
-    public init() { }
+    public init() {
+        let observer = QueryManager<Result>(database: nil)
+        _observer = StateObject(wrappedValue: observer)
+    }
+
+    public init(filter: @escaping (Result) -> Bool) {
+        let observer = QueryManager<Result>(database: nil, filter: filter)
+        _observer = StateObject(wrappedValue: observer)
+    }
+
+    public init(filter: @escaping (Result) -> Bool, sort: @escaping (Result, Result) -> Bool) {
+        let observer = QueryManager<Result>(database: nil, filter: filter, order: sort)
+        _observer = StateObject(wrappedValue: observer)
+    }
+
+    public init(sort: @escaping (Result, Result) -> Bool) {
+        let observer = QueryManager<Result>(database: nil, filter: nil, order: sort)
+        _observer = StateObject(wrappedValue: observer)
+    }
+
+    public init<T: Comparable>(sort order: QuerySortOrder = .ascending, using: @escaping (Result) -> T) {
+        let sort: (Result, Result) -> Bool
+        switch order {
+        case .ascending: sort = { using($0) < using($1) }
+        case .descending: sort = { using($0) > using($1) }
+        }
+        let observer = QueryManager<Result>(database: nil, filter: nil, order: sort)
+        _observer = StateObject(wrappedValue: observer)
+    }
+
+    public init<T: Comparable>(filter: @escaping (Result) -> Bool, sort order: QuerySortOrder = .ascending, using: @escaping (Result) -> T) {
+        let sort: (Result, Result) -> Bool
+        switch order {
+        case .ascending: sort = { using($0) < using($1) }
+        case .descending: sort = { using($0) > using($1) }
+        }
+        let observer = QueryManager<Result>(database: nil, filter: filter, order: sort)
+        _observer = StateObject(wrappedValue: observer)
+    }
 
     public var wrappedValue: [Result] {
         observer.results
--- Sources/StateModel/Property Wrappers/QueryManager.swift
+++ Sources/StateModel/Property Wrappers/QueryManager.swift
@@ -5,10 +5,17 @@ final class QueryManager<Result: ModelProtocol>: QueryObserver {
 
     var results: [Result] = []
 
+    /// The filter to apply to the instances
+    let filter: ((Result) -> Bool)?
+
+    let isOrderedBefore: ((Result, Result) -> Bool)?
+
     weak var database: ObservableDatabase?
 
-    init(database: ObservableDatabase?) {
+    init(database: ObservableDatabase?, filter: ((Result) -> Bool)? = nil, order: ((Result, Result) -> Bool)? = nil) {
         self.database = database
+        self.filter = filter
+        self.isOrderedBefore = order
     }
 
     func update(database: ObservableDatabase) {
@@ -16,13 +23,34 @@ final class QueryManager<Result: ModelProtocol>: QueryObserver {
             return
         }
         self.database = database
-        results = database.queryAll(observer: self, where: { _ in true })
+        let results: [Result] = database.queryAll(observer: self, where: { _ in true })
+        if let filter {
+            if let isOrderedBefore {
+                self.results = results.filter(filter).sorted(by: isOrderedBefore)
+            } else {
+                self.results = results.filter(filter)
+            }
+        } else if let isOrderedBefore {
+            self.results = results.sorted(by: isOrderedBefore)
+        } else {
+            self.results = results
+        }
     }
 
-    override func didUpdate(instance: InstanceKey) {
-        guard results.contains(where: { $0.id == instance }) else {
-            handleNewInstance(id: instance)
+    override func didUpdate(instance id: InstanceKey) {
+        guard let index = results.firstIndex(where: { $0.id == id }) else {
+            handleNewInstance(id: id)
             return
+        }
+        let instance = results[index]
+        if let filter, !filter(instance) {
+            results.remove(at: index)
+        }
+        if let isOrderedBefore {
+            let newIndex = results.firstIndex { !isOrderedBefore($0, instance) } ?? results.endIndex
+            if newIndex != index {
+                results.move(fromOffsets: IndexSet(integer: index), toOffset: newIndex)
+            }
         }
         self.objectWillChange.send()
     }
@@ -32,9 +60,26 @@ final class QueryManager<Result: ModelProtocol>: QueryObserver {
             return
         }
         guard let instance: Result = database.get(id: id) else {
+            remove(instance: id)
             return
         }
-        results.append(instance)
+        // Filter out instances
+        if let filter, !filter(instance) {
+            return
+        }
+        // Insert new instance
+        if let isOrderedBefore {
+            let insertIndex = results.firstIndex { !isOrderedBefore($0, instance) } ?? results.endIndex
+            results.insert(instance, at: insertIndex)
+        } else {
+            results.append(instance)
+        }
         self.objectWillChange.send()
+    }
+
+    private func remove(instance id: InstanceKey) {
+        if let index = results.firstIndex(where: {$0.id == id }) {
+            results.remove(at: index)
+        }
     }
 }
