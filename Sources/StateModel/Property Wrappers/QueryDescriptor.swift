c2940773c11474469a85284dce2caf259a3a3695
Christoph Hagen <github@christophhagen.de>
2025-10-03 11:09:58 +0200

    Additional descriptor initializers

--- Sources/StateModel/Property Wrappers/QueryDescriptor.swift
+++ Sources/StateModel/Property Wrappers/QueryDescriptor.swift
@@ -7,11 +7,40 @@ public struct QueryDescriptor<Result: ModelProtocol> {
 
     let areInIncreasingOrder: ((Result, Result) -> Bool)?
 
+    /**
+     Create a descriptor to configure a query.
+     - Parameter isIncluded: Optional closure to filter instances
+     - Parameter areInIncreasingOrder: Optional closure to sort the instances
+     */
     public init(filter isIncluded: ((Result) -> Bool)? = nil,
                 sort areInIncreasingOrder: ((Result, Result) -> Bool)? = nil) {
         self.id = .random(in: Int.min...Int.max)
         self.isIncluded = isIncluded
         self.areInIncreasingOrder = areInIncreasingOrder
+    }
+
+    /**
+     Create a descriptor to configure a query.
+     - Parameter isIncluded: Optional closure to filter instances
+     - Parameter order: The sort ordering for the instances
+     - Parameter transform: Transform each instance to a value that is used for sorting
+     */
+    public init<T: Comparable>(filter isIncluded: ((Result) -> Bool)? = nil, sort order: QuerySortOrder, using transform: @escaping (Result) -> T) {
+        switch order {
+        case .ascending:
+            self.init(filter: isIncluded, sort: { transform($0) < transform($1) })
+        case .descending:
+            self.init(filter: isIncluded, sort: { transform($0) > transform($1) })
+        }
+    }
+
+    /**
+     Create a descriptor to configure a query.
+     - Parameter isIncluded: Optional closure to filter instances
+     - Parameter transform: Transform each instance to a value that is used for sorting
+     */
+    public init<T: Comparable>(filter isIncluded: ((Result) -> Bool)? = nil, sortBy transform: @escaping (Result) -> T) {
+        self.init(filter: isIncluded, sort: .ascending, using: transform)
     }
 }
 
